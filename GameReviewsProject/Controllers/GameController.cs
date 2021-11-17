772c9006a7ca8c027a22895d2aeabb2816b24d90
Justin Grimes <justin@exceedinglynerdy.com>
2021-11-17 08:40:04 -0600

    added logging and fixed GetGame to deal with no item found with id

--- GameReviewsProject/Controllers/GameController.cs
+++ GameReviewsProject/Controllers/GameController.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using GameReviewSolution.DTOs;
+using GameReviewSolution.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace GameReviewSolution.Controllers;
@@ -13,18 +16,26 @@ public class GameController : ControllerBase
 {
     private readonly GameReviewContext _context;
     private readonly ILogger<GameController> _logger;
+
     public GameController(GameReviewContext context, ILogger<GameController> logger)
     {
         _context = context;
-        this._logger = logger;
+        _logger = logger;
     }
 
     [HttpGet]
     [Route("/{id?}")]
-    public Task<ActionResult<GameDto>> GetGame(int id)
+    public ActionResult<GameDto> GetGame(int id)
     {
-        var game = _context.Games.FirstOrDefault(g => g.Id == id);
-        return Task.FromResult<ActionResult<GameDto>>(game is not null ? game.ToDto() : NotFound());
+        var gameQuery = from g in _context.Games
+            where g.Id == id
+            select g.ToDto();
+        var game = gameQuery.FirstOrDefault();
+        if (game is null)
+        {
+            _logger.LogDebug("Couldn't locate game with {Id}", id);
+            return NotFound();
+        }
+        return game;
     }
-    
 }
