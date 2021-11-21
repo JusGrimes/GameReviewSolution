3e2d492a9287011c6addbc6274d8ee4e01292da4
Justin Grimes <justin@exceedinglynerdy.com>
2021-11-20 18:49:23 -0600

    updated project to .net 6 and updated GameController.cs to be Async.

--- GameReviewsProject/Controllers/GameController.cs
+++ GameReviewsProject/Controllers/GameController.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Data.Entity;
+
 using System.Linq;
 using System.Threading.Tasks;
 using GameReviewSolution.DTOs;
-using GameReviewSolution.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -25,17 +23,17 @@ public class GameController : ControllerBase
 
     [HttpGet]
     [Route("/{id?}")]
-    public ActionResult<GameDto> GetGame(int id)
+    public async Task<IActionResult> GetGame(int id)
     {
         var gameQuery = from g in _context.Games
             where g.Id == id
             select g.ToDto();
-        var game = gameQuery.FirstOrDefault();
+        var game = await gameQuery.SingleOrDefaultAsync();
         if (game is null)
         {
             _logger.LogDebug("Couldn't locate game with {Id}", id);
             return NotFound();
         }
-        return game;
+        return Ok(game);
     }
 }
