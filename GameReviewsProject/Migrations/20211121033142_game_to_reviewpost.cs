6b455c775d8738fcdaf342c83fa8b93785a1dd2d
Justin Grimes <justin@exceedinglynerdy.com>
2021-11-21 20:35:19 -0600

    ted modelupda

---
+++ GameReviewsProject/Migrations/20211121033142_game_to_reviewpost.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameReviewSolution.Migrations
+{
+    public partial class game_to_reviewpost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
