db35150f1e1a0e14283ed3a91042f5dc7b2e98b1
Justin Grimes <justin@exceedinglynerdy.com>
2021-11-26 16:01:49 -0600

    add uri checking

--- GameReviewsProject/Validators/GameDtoValidator.cs
+++ GameReviewsProject/Validators/GameDtoValidator.cs
@@ -8,15 +8,8 @@ public class GameDtoValidator : AbstractValidator<GameDto>
 {
     public GameDtoValidator()
     {
-        RuleFor(dto => dto.PublisherWebsiteUri).Must(ValidateUri);
-        RuleFor(dto => dto.PublisherWebsiteUri).Must(ValidateUri);
+        RuleFor(dto => dto.PublisherWebsiteUri).Must(UriHelper.IsValidUri);
+        RuleFor(dto => dto.PublisherWebsiteUri).Must(UriHelper.IsValidUri);
     }
 
-    public bool ValidateUri(string uri)
-    {
-        return Uri.TryCreate(uri, UriKind.Absolute, out var uriCreated)
-               && (
-                   uriCreated.Scheme == Uri.UriSchemeHttp ||
-                   uriCreated.Scheme == Uri.UriSchemeHttps);
-    }
 }
---
+++ GameReviewsProject/Validators/UriHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameReviewSolution.Validators;
+
+public static class UriHelper
+{
+    public static bool IsValidUri(string uri)
+    {
+        return Uri.TryCreate(uri, UriKind.Absolute, out var uriCreated)
+               && (
+                   uriCreated.Scheme == Uri.UriSchemeHttp ||
+                   uriCreated.Scheme == Uri.UriSchemeHttps);
+    }
+
+}
