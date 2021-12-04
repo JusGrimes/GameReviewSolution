d15f6d55d934bd5b43b9880dd1dcf93ee9735ee6
Justin Grimes <justin@exceedinglynerdy.com>
2021-12-04 16:34:26 -0600

    updated Street Address to use ID.

--- GameReviewsProject/Models/StreetAddress.cs
+++ GameReviewsProject/Models/StreetAddress.cs
@@ -5,16 +5,7 @@ namespace GameReviewSolution.Models;
 
 public class StreetAddress
 {
-    public StreetAddress(string firstLine, string? secondLine, string city, string state, int zipcode)
-    {
-        FirstLine = firstLine;
-        SecondLine = secondLine;
-        City = city;
-        State = state;
-        Zipcode = zipcode;
-    }
-
-    public string Id { get; set; } = null!;
+    public int Id { get; set; }
     public string FirstLine { get; set; }
     public string? SecondLine { get; set; }
     public string City { get; set; }
